d3c88f8514c799ec86759e07f04c818325798cb6
Alexander Schepanovski <suor.web@gmail.com>
2012-10-17 21:54:03 +0800

    Upped version to 0.2 and status to Beta

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='whatever',
-    version='0.1',
+    version='0.2',
     author='Alexander Schepanovski',
     author_email='suor.web@gmail.com',
 
@@ -14,7 +14,7 @@ setup(
     py_modules=['whatever'],
 
     classifiers=[
-        'Development Status :: 3 - Alpha',
+        'Development Status :: 4 - Beta',
         'License :: OSI Approved :: BSD License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
